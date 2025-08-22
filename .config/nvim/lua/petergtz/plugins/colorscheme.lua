5784849192dd0e0b4dcdc6cc32afeb205dd05b16
Pedro Gutierrez <peter.gtz2020@gmail.com>
2025-08-21 18:03:55 -0700

    Changed color scheme

--- .config/nvim/lua/petergtz/plugins/colorscheme.lua
+++ .config/nvim/lua/petergtz/plugins/colorscheme.lua
@@ -1,7 +1,16 @@
+-- return {
+--     "rebelot/kanagawa.nvim",
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     config = function()
+--         vim.cmd("colorscheme kanagawa-dragon")
+--     end
+-- }
+
 return {
-    "rebelot/kanagawa.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-        vim.cmd("colorscheme kanagawa-dragon")
-    end
+  "vague2k/vague.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other plugins
+  config = function()
+    vim.cmd("colorscheme vague")
+  end
 }
